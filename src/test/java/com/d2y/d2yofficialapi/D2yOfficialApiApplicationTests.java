747923ccc850e9e78ff1ccb9d58a542d7d817907
AdiMunawar31 <munawar.adi31@gmail.com>
2023-05-27 16:44:01 +0700

    initial commit

---
+++ src/test/java/com/d2y/d2yofficialapi/D2yOfficialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d2y.d2yofficialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2yOfficialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
